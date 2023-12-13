7e46594596d70202a59639d3df5b1c9a1533ba46
Chris <conley.christopher0@gmail.com>
2023-12-13 14:41:31 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+console.log(bestCustomer);
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+console.log(bestCustomer);
+
+const leastFavoriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob';
+}
+
